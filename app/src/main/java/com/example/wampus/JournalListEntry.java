221578273ce324611e26a56ce6cec574da488794
Anyi Lin <anyo3223@gmail.com>
2024-09-28 15:41:50 -0400

    entries done

--- app/src/main/java/com/example/wampus/JournalListEntry.java
+++ app/src/main/java/com/example/wampus/JournalListEntry.java
@@ -1,4 +1,76 @@
 package com.example.wampus;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.wampus.storage.Journal;
+import com.google.android.material.chip.Chip;
+
 public class JournalListEntry {
+    private Journal journal;
+
+    public JournalListEntry(Journal journal) {
+        this.journal = journal;
+    }
+
+    public View getListEntry(Context context, Runnable onClickListener) {
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 180));
+        layout.setOnClickListener(v -> onClickListener.run());
+
+        LinearLayout dateLayout = new LinearLayout(context);
+        dateLayout.setOrientation(LinearLayout.VERTICAL);
+        dateLayout.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 2));
+
+        TextView dateTextView = new TextView(context);
+        dateTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
+        dateTextView.setGravity(Gravity.BOTTOM | Gravity.CENTER);
+        dateTextView.setTextAlignment(TextView.TEXT_ALIGNMENT_GRAVITY);
+        dateTextView.setText(Integer.valueOf(journal.date.substring(0, 2)) + "/" + Integer.valueOf(journal.date.substring(3, 5)));
+        dateTextView.setTextSize(20);
+        dateTextView.setTypeface(null, android.graphics.Typeface.BOLD);
+        dateLayout.addView(dateTextView);
+
+        TextView dayTextView = new TextView(context);
+        dayTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
+        dayTextView.setGravity(Gravity.TOP | Gravity.CENTER);
+        dayTextView.setTextAlignment(TextView.TEXT_ALIGNMENT_GRAVITY);
+        dayTextView.setText(journal.dayOfWeek.substring(0, 3));
+        dayTextView.setTextSize(18);
+        dateLayout.addView(dayTextView);
+
+        layout.addView(dateLayout);
+
+
+        LinearLayout entryLayout = new LinearLayout(context);
+        entryLayout.setOrientation(LinearLayout.VERTICAL);
+        entryLayout.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 8));
+
+        TextView moodTextView = new TextView(context);
+        moodTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,1));
+        moodTextView.setBackgroundColor(android.graphics.Color.parseColor("#C7C7C7"));
+        moodTextView.setPadding(10,0,10,0);
+        moodTextView.setText(journal.mood);
+        entryLayout.addView(moodTextView);
+
+        TextView entryTextView = new TextView(context);
+        entryTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,0,2));
+        entryTextView.setBackgroundColor(android.graphics.Color.parseColor("#C7C7C7"));
+        entryTextView.setEllipsize(android.text.TextUtils.TruncateAt.END);
+        entryTextView.setMaxLines(2);
+        entryTextView.setPadding(10,0,10,0);
+        entryTextView.setText(journal.journalEntry);
+        entryLayout.addView(entryTextView);
+
+        layout.addView(entryLayout);
+
+        return layout;
+    }
 }
