811b50b7b9a5790d8dc28a872268ff8789aa1f0d
Anyi Lin <anyo3223@gmail.com>
2024-09-27 23:44:11 -0400

    foundation done

---
+++ app/src/main/java/com/example/wampus/storage/JournalDAO.java
@@ -0,0 +1,36 @@
+package com.example.wampus.storage;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface JournalDAO {
+    @Insert
+    void insert(Journal journal);
+
+    @Update
+    void update(Journal journal);
+
+    @Delete
+    void delete(Journal journal);
+
+    @Query("SELECT * FROM journal_table WHERE date = :date")
+    Journal getJournalByDate(String date);
+
+    @Query("SELECT * FROM journal_table WHERE day_of_week = :dayOfWeek")
+    Journal getJournalByDayOfWeek(String dayOfWeek);
+
+    @Query("SELECT * FROM journal_table WHERE journal_title = :title")
+    Journal getJournalByTitle(String title);
+
+    @Query("SELECT * FROM journal_table WHERE prompt = :prompt")
+    Journal getJournalByPrompt(String prompt);
+
+    @Query("SELECT * FROM journal_table")
+    List<Journal> getAllJournals();
+}
---
+++ app/src/main/java/com/example/wampus/storage/JournalDatabaseAccessor.java
@@ -0,0 +1,38 @@
+package com.example.wampus.storage;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import java.util.List;
+
+public class JournalDatabaseAccessor {
+    private JournalDatabase database;
+    private JournalDAO dao;
+
+    public JournalDatabaseAccessor(Context context) {
+        database = Room.databaseBuilder(context,
+                JournalDatabase.class, "journal_table").build();
+        dao = database.journalDAO();
+    }
+
+    public List<Journal> getAllJournals() {
+        return dao.getAllJournals();
+    }
+
+    public Journal getJournalByDate(String date) {
+        return dao.getJournalByDate(date);
+    }
+
+    public Journal getJournalByDayOfWeek(String dayOfWeek) {
+        return dao.getJournalByDayOfWeek(dayOfWeek);
+    }
+
+    public Journal getJournalByTitle(String title) {
+        return dao.getJournalByTitle(title);
+    }
+
+    public Journal getJournalByPrompt(String prompt) {
+        return dao.getJournalByPrompt(prompt);
+    }
+}
